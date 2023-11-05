d46fb343088a235873bf316077c4555fcfea7998
Luis <luisjavier_2705@hotmail.com>
2023-11-05 08:12:07 -0500

    proyecto final

--- src/main/java/com/proyecto/Proyecto/Exceptions/ExceptionHandlerController.java
+++ src/main/java/com/proyecto/Proyecto/Exceptions/ExceptionHandlerController.java
@@ -1,8 +1,10 @@
 package com.proyecto.Proyecto.Exceptions;
 
+import org.apache.tomcat.util.http.fileupload.impl.FileSizeLimitExceededException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -16,5 +18,17 @@ public class ExceptionHandlerController {
         response.put("message", ex.getMessage());
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
+    @ExceptionHandler(FileSizeLimitExceededException.class)
+    public ResponseEntity<HashMap<String,Object>> handleHttpMessageFileSizeLimitExceededException(FileSizeLimitExceededException ex) {
+        HashMap<String, Object> response = new HashMap<>();
+        response.put("message", ex.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
 
+    @ExceptionHandler(UsernameNotFoundException.class)
+    public ResponseEntity<HashMap<String,Object>> handleHttpMessageUsernameNotFoundException(UsernameNotFoundException ex) {
+        HashMap<String, Object> response = new HashMap<>();
+        response.put("message", ex.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
 }
