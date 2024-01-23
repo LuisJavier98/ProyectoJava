1f4d9f56317c97a6a28dd184397d0a1e61cfb76a
Luis <luisjavier_2705@hotmail.com>
2024-01-22 21:29:13 -0500

    add images to static file

--- src/main/java/com/proyecto/Proyecto/ProyectoCodigoApplication.java
+++ src/main/java/com/proyecto/Proyecto/ProyectoCodigoApplication.java
@@ -4,20 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @SpringBootApplication
 @EnableMethodSecurity
 @EnableJpaAuditing
-public class ProyectoCodigoApplication  implements WebMvcConfigurer {
-	public static void main(String[] args) {
-		SpringApplication.run(ProyectoCodigoApplication.class, args);
-	}
-	@Override
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/images/**")
-				.addResourceLocations("file:images/");
-	}
+public class ProyectoCodigoApplication implements WebMvcConfigurer {
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoCodigoApplication.class, args);
+    }
 }
+
